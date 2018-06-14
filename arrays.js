d68c08ebbf3f483eb42d53a5c46db1ad7845010c
bilojan91@gmial.com <bilojan91@gmial.com>
2018-06-14 17:02:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,2 @@
-function addElementToBeginningOfArray (){}
-Function  destructivelyAddElementToBeginningOfArray(){}
+function addElementToBeginningOfArray (var mara[3],element){}
+function  destructivelyAddElementToBeginningOfArray(dara[5],element){}
